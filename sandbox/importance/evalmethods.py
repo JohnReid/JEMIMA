5d98eb2d56df8df7d62f015663f2d412a57b3e75
JohnReid <johnbaronreid@netscape.net>
2014-07-18 11:07:17 +0100

    Seed RNGs in parallel

--- sandbox/importance/evalmethods.py
+++ sandbox/importance/evalmethods.py
@@ -24,6 +24,8 @@ import matplotlib.pyplot as plt
 import numpy as npy
 import numpy.random as rdm
 import argparse
+import functools
+import sys
 
 
 parser = argparse.ArgumentParser(
@@ -31,7 +33,7 @@ parser = argparse.ArgumentParser(
 args = parser.parse_args(args=[])
 
 
-rdm.seed(1)
+args.rngseed = 1
 args.fastas = ['T00759-small.fa']
 args.Ws = (6, 8, 11, 14)
 args.numseeds = 100
@@ -43,29 +45,52 @@ args.methods = [
     'uniformweights',
 ]
 args.writelogos = False
+args.parallel = True
 
 
 #
 # Initialise parallel
 #
-from IPython.parallel import Client
-rc = Client()
-lview = rc.load_balanced_view()
-lview.block = True
+if args.parallel:
+    try:
+        from IPython.parallel import Client
+        rc = Client()
+        lview = rc.load_balanced_view()
+        lview.block = True
 
+        #
+        # Initialise RNGs on each engine
+        #
+        logger.info('Initialising engine RNGs.')
+        def initrng(rngseed):
+            import numpy.random
+            numpy.random.seed(rngseed)
+        for engineidx, engine in enumerate(rc):
+            engine.apply(initrng, args.rngseed + engineidx)
 
-@lview.parallel()
-def doseed(seedidx):
-    import jemima.evaluation
-    return jemima.evaluation.doseed(seedidx, args)
+        @lview.parallel()
+        def doseed(seedidx):
+            import jemima.evaluation
+            return jemima.evaluation.doseed(seedidx, args)
 
+        #
+        # Pass work to engines
+        #
+        logger.info('Passing work to engines.')
+        dview = rc[:]
+        dview.push({'args': args})
+        result = doseed.map(xrange(args.numseeds))
 
-#
-# Pass work to engines
-#
-dview = rc[:]
-dview.push({'args': args})
-result = doseed.map(xrange(args.numseeds))
+    except Exception as e:
+        logger.warning("Could not evaluate in parallel:\n%s", e)
+        args.parallel = False
+        sys.exit(-1)
+
+if not args.parallel:
+    rdm.seed(args.rngseed)
+    result = map(
+        functools.partial(jemima.evaluation.doseed, args=args),
+        xrange(args.numseeds))
 
 statsdf = pds.concat(map(pds.DataFrame, result))
 statsdf.to_pickle('statsdf.pkl')
