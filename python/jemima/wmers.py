b12803b825d3d6499cf7654ef248dac29b67f391
JohnReid <johnbaronreid@netscape.net>
2014-07-19 17:19:50 +0100

    Refactor W-mer counting and improve docs

--- python/jemima/wmers.py
+++ python/jemima/wmers.py
@@ -4,10 +4,11 @@
 
 
 r"""
-In general when finding motifs we need to consider several different motif
-widths, W.  When descending a suffix tree or array we typically need to know
-how many W-mers for a given W exist are descendants of the current iterator.
-The code in this module counts these W-mers.
+When iterating over all the unique :math:`W`-mers in a text or their
+occurrences, we often need to know the total number of such :math:`W`-mers
+below any given vertex in a suffix tree (or array).
+This module provides functionality to count unique :math:`W`-mers or their
+occurrences.
 
 Counting example
 ----------------
@@ -22,7 +23,8 @@ Build an index from some short sequences.
 
 .. doctest::
 
-    >>> seqs = seqan.StringDNA5Set(('ACGT', 'AAAAA', 'AAGG', 'AAGG', 'AC', 'AGNNCCC'))
+    >>> seqs = seqan.StringDNA5Set(
+    ...     ('ACGT', 'AAAAA', 'AAGG', 'AAGG', 'AC', 'AGNNCCC'))
     >>> index = seqan.IndexStringDNA5SetESA(seqs)
 
 Count W-mers of lengths 2, 3 and 5 (we should have seventeen 2-mers, ten 3-mers
@@ -32,7 +34,8 @@ and one 5-mer).
 
     >>> Ws = [2, 3, 5]
     >>> counts = numpy.zeros(((2*len(index)), len(Ws)), dtype=numpy.uint)
-    >>> print jemima.wmers.countWmersMulti(index.topdownhistory(), Ws, counts)
+    >>> print jemima.wmers.countOccsMulti(
+    ...     index.topdownhistory(), Ws, counts, countUnique=False)
     [17 10  1]
 
 Six 2-mers, five 3-mers and one 5-mer start with 'AA'
@@ -62,7 +65,7 @@ frequencies of each possible subsequent base.
 .. doctest::
 
     >>> childcounts = numpy.zeros(((2*len(index)), len(Ws), 4))
-    >>> jemima.wmers.countWmerChildren(
+    >>> jemima.wmers.countChildren(
     ...     index.topdownhistory(), Ws, counts, childcounts)
 
 Examine the distribution of bases in all W-mers (for W=2, 3 and 5) after the
@@ -74,55 +77,33 @@ prefix 'AA'
     >>> it.goDown('AA')
     True
     >>> print childcounts[it.value.id]
-    [[ 3.  0.  2.  0.]
+    [[ 0.  0.  0.  0.]
      [ 3.  0.  2.  0.]
      [ 1.  0.  0.  0.]]
 
-For example, three of the 2-mers starting with 'AA' are followed by 'A'.  Two
-of the 2-mers starting with 'AA' are followed by 'G'.
+For example, none of the 2-mers starting with 'AA' are followed by anything.
+Three of the 3-mers starting with 'AA' are followed by 'A' and two by 'G'.
+The only 5-mer starting with 'AA' is followed by 'A'.
 
 """
 
 
 import bisect
-from . import UNKNOWNBASE, parentEdgeLabelUpToW, findfirstparentunknown
+from . import UNKNOWNBASE, findfirstparentunknown
 
 
-def countWmers(it, W, counts):
+def countOccsMulti(it, Ws, counts, countUnique=True):
+    """Count all the occurrences below the iterator for multiple widths, Ws.
+
+    Arguments:
+        - *it*: The iterator below which to count occurrences.
+        - *Ws*: The widths to count for.
+        - *counts*: The counts array of shape
+            (2*len(index), len(Ws), jem.SIGMA)
+        - *countUnique*: If true, count the number of unique W-mers below
+            each vertex for each width. Otherwise count the number of
+            occurrences.
     """
-    Count how many W-mers are descendants of this iterator.
-
-    Args:
-        - it: A top-down history iterator.
-        - W: The length of the W-mers we are counting.
-        - counts: An array to place the counts in.
-
-    The counts array is typically initialised as::
-
-        counts = [0] * (2 * len(index))
-
-    and is indexed by::
-
-        counts[it.value.id]
-    """
-    if it.repLength >= W:
-        count = it.numOccurrences
-    else:
-        count = 0
-        if it.goDown():
-            while True:
-                if UNKNOWNBASE not in parentEdgeLabelUpToW(it, W):
-                    count += countWmers(it, W, counts)
-                if not it.goRight():
-                    break
-            it.goUp()
-    counts[it.value.id] = count
-    return count
-
-
-def countWmersMulti(it, Ws, counts):
-    """Count all the W-mers below the iterator for the given
-    widths, Ws."""
     nodecounts = counts[it.value.id]
     maxW = Ws[-1]
     firstunknown = findfirstparentunknown(it, maxW)
@@ -132,21 +113,28 @@ def countWmersMulti(it, Ws, counts):
         # Yes we should descend so go down and add up counts from child nodes
         if it.goDown():
             while True:
-                nodecounts += countWmersMulti(it, Ws, counts)
+                nodecounts += countOccsMulti(it, Ws, counts, countUnique)
                 if not it.goRight():
                     break
             it.goUp()
     # Determine which Ws our representative is longer than
     longestWidx = bisect.bisect(Ws, firstunknown)
+    # Determine which Ws our parent representative is longer than
+    parentWidx = not it.isRoot and bisect.bisect(
+        Ws[:longestWidx],
+        it.repLength - it.parentEdgeLength) or 0
     # Set those counts to number of occurrences
-    nodecounts[:longestWidx] = it.numOccurrences
-    # print it.representative, longestWidx, it.numOccurrences, firstunknown, nodecounts
+    if countUnique:
+        nodecounts[parentWidx:longestWidx] = 1
+    else:
+        nodecounts[parentWidx:longestWidx] = it.numOccurrences
     return nodecounts
 
 
-def countWmerChildren(it, Ws, counts, childcounts):
+def countChildren(it, Ws, counts, childcounts):
     """
-    Count how many Wmers are descendants of each child of each node.
+    Given a set of counts for given widths, summarise how many counts are
+    beneath each node.
 
     Args:
         - it: A top-down history iterator.
@@ -170,7 +158,7 @@ def countWmerChildren(it, Ws, counts, childcounts):
                 if UNKNOWNBASE != it.parentEdgeFirstChar:
                     nodechild[:, it.parentEdgeFirstChar.ordValue] += \
                         counts[it.value.id]
-                    countWmerChildren(it, Ws, counts, childcounts)
+                    countChildren(it, Ws, counts, childcounts)
                 if not it.goRight():
                     break
             it.goUp()
